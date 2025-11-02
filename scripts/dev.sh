d3aa3d00d9323288a045bb094f6615e149956258
Sonya-M <sonja.musicki@gmail.com>
2025-11-02 22:24:15 +0100

    dev mode

---
+++ scripts/dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pnpm dlx  concurrently -n "Server,Models,Client" \
+             -c "yellow,green,blue" \
+              "pnpm --filter=@seeds/server run dev" \
+              "pnpm --filter=@seeds/models run dev" \
+              "pnpm --filter=@seeds/ui run dev" \
